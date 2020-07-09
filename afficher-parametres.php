9143970412103246c0534ddf47d8e52f63577204
grigoryk99 <67262259+grigoryk99@users.noreply.github.com>
2020-07-09 11:04:30 +0300

    Add files via upload

---
+++ afficher-parametres.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+<head>
+<nav class = "menu">
+<ul>
+  <li><a href="mini-site-routing.php">Accueil</a>
+  <li><a href="mini-site-routing.php">Page1</a> 
+  <li><a href="mini-site-routing.php">Page2</a>
+  <li><a href="mini-site-routing.php">Page3</a>
+  </ul>
+</nav>
+<title1> mini-site-routing </title>
+</html>
+</body>
---
+++ mini-site-routing.php
@@ -0,0 +1,22 @@
+
+<?php
+$page1 = "Page ";
+$page2 = "Page ";
+$page3 = "Page ";
+$a = '1';
+$b = '2';
+$c = '3';
+$page_number1 = $page1.$a;
+$page_number2 = $page2.$b;
+$page_number3 = $page3.$c;
+ 
+if ($page_number1 = $a);
+ echo "Acceuil!";
+
+ if ($page_number2 = $b);
+ echo "Page 2!";
+
+ if ($page_number3 = $a);
+ echo "Page 3";
+?>
+
